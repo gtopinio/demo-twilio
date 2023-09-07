c446fc65a4c86595d36b78ccfce4018e62bc643f
gtopinio <mctopinio@up.edu.ph>
2023-09-07 23:17:49 +0800

    update: initialize twilio sms sender

---
+++ src/main/java/com/gtopinio/demotwilio/TwilioSmsSender.java
@@ -0,0 +1,44 @@
+package com.gtopinio.demotwilio;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwilioSmsSender implements SmsSender {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
+    private final TwilioConfiguration twilioConfiguration;
+
+    @Autowired
+    public TwilioSmsSender(TwilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
+    }
+
+    @Override
+    public void sendSms(SmsRequest smsRequest) {
+        if (isPhoneNumberValid(smsRequest.getPhoneNumber())) {
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber("");
+            String message = smsRequest.getMessage();
+            Message.creator(
+                    to,
+                    from,
+                    message
+            ).create();
+            LOGGER.info("Send sms: {}", smsRequest);
+        } else {
+            throw new IllegalArgumentException(
+                    "Phone number [" + smsRequest.getPhoneNumber() + "] is not a valid number"
+            );
+        }
+    }
+
+    private Boolean isPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.startsWith("09") && phoneNumber.length() == 11;
+    }
+
+}
