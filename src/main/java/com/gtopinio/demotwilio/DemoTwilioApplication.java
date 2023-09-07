e84a462504cffb152e9489ecca8da62f7483120a
gtopinio <mctopinio@up.edu.ph>
2023-09-07 22:28:08 +0800

    release: initial commit

---
+++ src/main/java/com/gtopinio/demotwilio/DemoTwilioApplication.java
@@ -0,0 +1,13 @@
+package com.gtopinio.demotwilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTwilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtopinio/demotwilio/DemoTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtopinio.demotwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
